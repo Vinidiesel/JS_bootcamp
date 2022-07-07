336b9d53738c5bd614d07b9d5b181dea202b871c
Vinicius Inhesta Dos Santos <91141292+Vinidiesel@users.noreply.github.com>
2022-07-07 20:33:31 -0300

    Update Pares.js

--- JS_Variaveis/Pares.js
+++ JS_Variaveis/Pares.js
@@ -1,4 +1,6 @@
 function substituiPares(array){
+    if(!array) return -1;
+    if(!array.lenght) return -1;
     for(let i=0; i < array.length; i++){
         if(array[i] === 0){
             console.log("Voce já é zero");
